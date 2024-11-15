9211091b0bb9992c56beac9542969c6c3f14c8ac
DataPhaseDan <19226450+DataPhaseDan@users.noreply.github.com>
2024-11-15 18:41:27 +0100

    explicit convert of .ico

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component }: PageProps) {
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<link rel="stylesheet" href="/styles.css" />
-				<link rel="icon" href="static/favicon.ico" sizes="32x32"/>
+				
 			</head>
 			<div class="max-w-xl my-5 mx-auto lg:mx-2 sm:mx-1 text-sm text-white font-plex leading-none tracking-wide md:my-3">
 				<Component />
